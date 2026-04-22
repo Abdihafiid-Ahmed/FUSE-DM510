55cdd78c309e1709ba5ed65e520ce295acce8e11
Marius Brøgger <marbr24@student.sdu.dk>
2026-04-22 15:25:12 +0200

    header file

--- headers/inode.h
+++ headers/inode.h
@@ -1,9 +1,20 @@
 #ifndef INODE_H
 #define INODE_H
 
+#include <time.h>
 
+#define MAX_FILES 100
+#define MAX_FILENAME 32
+#define MAX_FILE_SIZE 4096
+
+typedef struct {
+    int in_use;
+    int is_dir;
+    char name[MAX_FILENAME];
+    int size;
+    char data[MAX_FILE_SIZE];
+    time_t last_modified;
+} inode_t;
 
 #endif
-
-
 
