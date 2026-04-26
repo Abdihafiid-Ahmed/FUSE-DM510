6302d6081daa8793a2040ecc893606e50744d1c5
Marius Brøgger <marbr24@student.sdu.dk>
2026-04-26 23:31:19 +0200

    Storage h

--- headers/storage.h
+++ headers/storage.h
@@ -1,4 +1,13 @@
 #ifndef STORAGE_H
 #define STORAGE_H
 
-#endif 
+// Path to the SD card partition
+#define DISK_PATH "/dev/mmcblk0p3"
+
+// Load the file system from disk to memory
+int storage_load(void);
+
+// Save the file system from memory to disk
+int storage_save(void);
+
+#endif
