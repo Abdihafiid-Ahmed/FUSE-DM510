34001016b63c4c21edee61b2228ef6aa862b3276
Marius Brøgger <marbr24@student.sdu.dk>
2026-04-26 17:33:54 +0200

    path

--- headers/path.h
+++ headers/path.h
@@ -1,6 +1,12 @@
 #ifndef PATH_H
 #define PATH_H
 
+#include "inode.h"
 
+// Find the inode index for a full path
+int get_inode(const char *path);
+
+// Find the parent folder's inode and get the file name
+int get_parent(const char *path, char *filename);
 
 #endif
